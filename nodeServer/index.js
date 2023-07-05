f2d95ce0a79c7a5772bc4cdeb46199be76eb8cfe
AryaPathak <99078194+AryaPathak@users.noreply.github.com>
2023-07-05 11:02:27 +0530

    Final Submit

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,17 +1,26 @@
-//Node server to handel socket.io
+// Node server which will handle socket io connections
+
 
 const io = require('socket.io')(8000)
 
 const users = {};
 
-io.on(`connection`, socket=>{
-    socket.on(`new-user-joined`, name=>{
+io.on('connection', socket =>{
+    
+    socket.on('new-user-joined', name =>{ 
         users[socket.id] = name;
-        socket.broadcast.emit(`user-joined`, name)
-    })
+        socket.broadcast.emit('user-joined', name);
+    });
 
-    socket.on(`send`, message =>{
-        socket.broadcast.emit('receive', {message:message, name:user[socket.id]})
-    })
+   
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+
 
 })
